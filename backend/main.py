4e303faa3d5e99479370ae9e56c53becc630d27a
AndyChiquin <andychiquindev@gmail.com>
2025-02-19 19:49:56 -0500

    feat(backend):update some files

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
 from services.generate_music import generate_music, get_audio
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app) 
 
 @app.route("/")
 def home():
--- backend/models/song.py
+++
@@ -1,6 +0,0 @@
-class Song:
-    def __init__(self, lyrics, genre, mood, audio_url=None):
-        self.lyrics = lyrics
-        self.genre = genre
-        self.mood = mood
-        self.audio_url = audio_url
