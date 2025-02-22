5f9cf5d9cbcfc76ed069da31388f05be3b64460b
JessJoha <jessicajcenteno@gmail.com>
2025-02-21 23:35:28 -0500

    feat(login): loginv3

--- backend/Login_Register/auth.py
+++ backend/Login_Register/auth.py
@@ -1,3 +1,4 @@
+
 from flask import Blueprint, session, redirect, url_for
 from Login_Register.auth_controller import register, login, google_callback
 from flask_login import login_required, logout_user
@@ -10,22 +11,18 @@ auth_bp.route('/register', methods=['POST'])(register)
 auth_bp.route('/login', methods=['POST'])(login)
 
 def init_oauth_routes(google):
-
-    
     @auth_bp.route('/google')
     def google_login():
         redirect_uri = url_for('auth.google_auth_callback', _external=True)
         return google.authorize_redirect(redirect_uri)
-    
+
     @auth_bp.route('/google/callback')
     def google_auth_callback():
         return google_callback(google)
-    
+
     @auth_bp.route('/logout')
     @login_required
     def logout():
         logout_user()
         session.clear()
         return redirect('/')
-
-
